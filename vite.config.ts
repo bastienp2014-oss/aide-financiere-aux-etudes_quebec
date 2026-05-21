a482ef5daeb0150a6b7696958a6eb098b576b97a
Patrice <bastienp2014@gmail.com>
2026-05-21 18:33:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/Aide-financi-re-aux-tudes/',
+    base: '/aide-financiere-aux-etudes_quebec/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
